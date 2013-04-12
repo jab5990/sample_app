3d945d2a474bf619f3237453bc208ed677bbf42e
Jose Barroso <jab5990@gmail.com>
2013-04-12 08:32:56 -0700

    User show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
 
-  get "users/new"
+  resources :users
 
   root :to => 'static_pages#home'
 
@@ -9,7 +9,7 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
 
   match '/signup', to: 'users#new'
-  
+
 
 
   # The priority is based upon order of creation:
